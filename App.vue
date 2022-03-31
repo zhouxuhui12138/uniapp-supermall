853d5d87b854d8119f3eeb81752cc671de387140
xiaozhou <zxh04551@163.com>
2022-03-31 21:15:00 +0800

    test

--- App.vue
+++ App.vue
@@ -1,24 +1,21 @@
 <script>
-	export default {
-		onLaunch: function() {
-		},
-		onShow: function() {
-		},
-		onHide: function() {
-		}
-	}
+export default {
+  onLaunch: function () {},
+  onShow: function () {},
+  onHide: function () {},
+};
 </script>
 
 <script>
-	export default {
-		globalData: {
-			text: '全局变量'
-		}
-	}
+export default {
+  globalData: {
+    text: "全局变量",
+  },
+};
 </script>
 
 <style lang="less">
-	// 引入全局css
-	@import "./static/css/normalize.css";
-	@import "./static/css/base.css";
+// 引入全局css
+@import "./static/css/normalize.css";
+@import "./static/css/base.css";
 </style>
